63727c932f2f55ef1b87adab38187a4f9fec31a5
Korey Nichols <koreynichols1@gmail.com>
2026-04-07 16:26:51 -0700

    lesson 1 completed

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,26 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Korey Nichols';
+  const age = '37';
+  const hobbies = [
+    { id: 1, title: 'Fishing' },
+    { id: 2, title: 'Gaming' },
+    { id: 3, title: 'Sports' },
+    { id: 4, title: 'Web Development' },
+  ];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About {name}</h1>
+      <p>
+        Hi, welcome to my page. My name is {name}. I am {age} years old from
+        California. My hobbies include:
+      </p>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby.id}>{hobby.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
