22673c567301e5616d0d203cb304a5112cd2b251
Korey Nichols <koreynichols1@gmail.com>
2026-04-14 18:53:16 -0700

    Created SnackApp and imported Header, List and Footer to display

---
+++ src/exercises/lesson-02/SnackApp.jsx
@@ -0,0 +1,13 @@
+import SnackHeader from './SnackHeader';
+import SnackList from './SnackList';
+import SnackFooter from './SnackFooter';
+
+export default function SnackApp() {
+  return (
+    <>
+      <SnackHeader></SnackHeader>
+      <SnackList></SnackList>
+      <SnackFooter></SnackFooter>
+    </>
+  );
+}
