02afb002867b1a73a694b47b629bbb755ee5a3cb
Korey Nichols <koreynichols1@gmail.com>
2026-04-28 16:18:33 -0700

    button click now sets focus to input

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,15 +1,22 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+import { useRef } from 'react';
 export default function FillRefFocus() {
-  function focusInput() {}
+  const inputRef = useRef();
+
+  function focusInput() {
+    inputRef.current.focus();
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
 
-      <input type="text" placeholder="Type here..." />
+      <input ref={inputRef} type="text" placeholder="Type here..." />
 
       <button onClick={focusInput}>Focus Input</button>
     </div>
   );
 }
+
+//added ref for input and set focus to the ref when clicking on button.
